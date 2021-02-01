3a0590951e8a39586687feba20b2b08ca7e117e0
doginnn <diogenesemmanuel@gmail.com>
2021-02-01 20:48:34 -0300

    [RFT]Refatorando models.py.

--- quiz_devpro/quiz/models.py
+++ quiz_devpro/quiz/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
+
 class Pergunta(models.Model):
     enunciado = models.TextField()
     alternativas = models.JSONField()
-    disponivel = models.BooleanField(default = False)
-    alternativa_correta = models.IntegerField(choices =[
+    disponivel = models.BooleanField(default=False)
+    alternativa_correta = models.IntegerField(choices=[
         (0, 'A'),
         (1, 'B'),
         (2, 'C'),
@@ -16,21 +17,21 @@ class Pergunta(models.Model):
 
 
 class Aluno(models.Model):
-    nome = models.CharField(max_length = 64)
-    email = models.EmailField(unique = True)
-    criacao = models.DateTimeField(auto_now_add = True)
+    nome = models.CharField(max_length=64)
+    email = models.EmailField(unique=True)
+    criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.email
 
 
 class Resposta(models.Model):
-    criacao = models.DateTimeField(auto_now_add = True)
+    criacao = models.DateTimeField(auto_now_add=True)
     pontos = models.IntegerField()
-    aluno = models.ForeignKey(Aluno, on_delete = models.CASCADE)
-    pergunta = models.ForeignKey(Pergunta, on_delete = models.CASCADE)
+    aluno = models.ForeignKey(Aluno, on_delete=models.CASCADE)
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
 
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields = ['aluno', 'pergunta'], name = 'resposta_unica')
+            models.UniqueConstraint(fields=['aluno', 'pergunta'], name='resposta_unica')
         ]
